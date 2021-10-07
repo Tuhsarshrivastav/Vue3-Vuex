2baba21123f184b8623154eae5fdf3a103ea52df
Tusharshrivastav <ts3657@gmail.com>
2021-10-07 14:03:15 +0530

    finsh

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="nav">
+    <Nav />
+  </div>
+  <router-view />
+</template>
+<script>
+import Nav from "./views/Nav"
+export default {
+  components: {
+    Nav
+  }
+}
+</script>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: sans-serif;
+  background: #c8ebdf;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    
+    <Names />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Names from "@/components/Names.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Names
+  }
+};
+</script>
